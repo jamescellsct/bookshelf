b118abb0099a9a1bd60de964a239ae59592abac2
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-27 17:12:31 -0600

    exercises/03-data-fetching

---
+++ src/discover.js
@@ -0,0 +1,3 @@
+export * from './discover.final'
+
+// export * from './discover.exercise'
--- src/screens/__tests__/not-found.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render} from 'test/app-test-utils'
-import {NotFound} from '../not-found'
-
-test('renders a go home link', () => {
-  const {getByText} = render(<NotFound />)
-  expect(getByText(/home/i).closest('a')).toHaveAttribute('href', '/')
-})
